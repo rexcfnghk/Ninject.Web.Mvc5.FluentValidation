9b966beb0112357597b83fb6a8b61999e1ae1af8
Rex Ng <rexng@thisisnoble.com>
2015-07-29 14:01:24 +0800

    Initial commit

---
+++ Ninject.Web.Mvc5.FluentValidation/NinjectValidatorFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+
+namespace Ninject.Web.Mvc5.FluentValidation
+{
+    /// <summary>
+    /// Validation factory that uses ninject to create validators  
+    /// </summary>
+    public class NinjectValidatorFactory : ValidatorFactoryBase
+    {
+        private readonly IKernel _kernel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NinjectValidatorFactory"/> class.
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        public NinjectValidatorFactory(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        /// <summary>
+        /// Creates an instance of a validator with the given type using ninject.
+        /// </summary>
+        /// <param name="validatorType">Type of the validator</param>
+        /// <returns>The newly created validator</returns>
+        public override IValidator CreateInstance(Type validatorType)
+        {
+            if (!_kernel.GetBindings(validatorType).Any())
+            {
+                return null;
+            }
+
+            return _kernel.Get(validatorType) as IValidator;
+        }
+    }
+}
