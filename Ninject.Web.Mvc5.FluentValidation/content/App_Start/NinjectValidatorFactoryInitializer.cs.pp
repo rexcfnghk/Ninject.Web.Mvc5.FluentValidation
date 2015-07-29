8efd07a0a5655e7afde343aad16ebd88c4cf6e77
Rex Ng <rexng@thisisnoble.com>
2015-07-29 14:23:47 +0800

    Add preprocessed initializer

---
+++ Ninject.Web.Mvc5.FluentValidation/content/App_Start/NinjectValidatorFactoryInitializer.cs.pp
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using FluentValidation.Mvc;
+using Ninject.Web.Mvc5.FluentValidation;
+using WebActivatorEx;
+
+[assembly: PreApplicationStartMethod(typeof($rootnamespace$.App_Start.NinjectValidatorFactoryInitializer), nameof(NinjectValidatorFactoryInitializer.PreStart))]
+namespace $rootnamespace$.App_Start
+{
+    public static class NinjectValidatorFactoryInitializer
+    {
+        public static void PreStart()
+        {
+            var ninjectValidatorFactory = new NinjectValidatorFactory(DependencyResolver.Current.GetService<IKernel>());
+            ModelValidatorProviders.Providers.Add(new FluentValidationModelValidatorProvider(ninjectValidatorFactory));
+            DataAnnotationsModelValidatorProvider.AddImplicitRequiredAttributeForValueTypes = false;
+        }
+    }
+}
